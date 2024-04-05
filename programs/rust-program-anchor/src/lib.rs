187aa698832a3f1743ba39b6257e2ca67553505e
gph <1598542650@qq.com>
2024-04-05 19:02:11 +0800

    update InitializeMintAccount

--- programs/rust-program-anchor/src/lib.rs
+++ programs/rust-program-anchor/src/lib.rs
@@ -1,4 +1,6 @@
 use anchor_lang::prelude::*;
+use anchor_spl::token::{mint_to, MintTo, Mint, TokenAccount, Token};
+use anchor_spl::associated_token::AssociatedToken;
 
 declare_id!("F7uwK9BXr7orrQWJujdoTVbd7P7yMDnPA84udYYyPPLM");
 
@@ -6,10 +8,63 @@ declare_id!("F7uwK9BXr7orrQWJujdoTVbd7P7yMDnPA84udYYyPPLM");
 pub mod rust_program_anchor {
     use super::*;
 
-    pub fn initialize(ctx: Context<Initialize>) -> Result<()> {
+    pub fn initialize(ctx: Context<InitializeMintAccount>,title: String, description: String, rating: u8) -> Result<()> {
         Ok(())
     }
 }
 
 #[derive(Accounts)]
-pub struct Initialize {}
+#[instruction(
+    id:u64,
+    description:String,
+    owner: String,
+    creator:String,
+    authorize: bool,
+    url: String,
+    is_initialized: bool,
+    cid: String,
+    is_mutable: bool,
+)]
+pub struct InitializeMintAccount<'info> {
+    #[account(
+        init,
+        seeds = ["mint".as_bytes()],
+        bump,
+        payer = initializer,
+        mint::decimals = 0,
+        mint::authority = mint,
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(
+        init,
+        payer = initializer,
+        space = 8 + 4 + description.len() + owner.len() + creator.len() + 4 + url.len() + 4 + cid.len() + 4,
+    )]
+    pub metadata_account: Account<'info,MetadataAccount>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub system_program: Program<'info, System>,
+    pub token_program: Program<'info, Token>,
+    #[account(
+        init_if_needed,
+        payer = initializer,
+        associated_token::mint = mint,
+        associated_token::authority = initializer
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[account]
+pub struct MetadataAccount {
+    pub id: u64,
+    pub description: String,
+    pub owner: String,
+    pub creator: String,
+    pub authorize: bool,
+    pub url: String,
+    pub is_initialized: bool,
+    pub cid: String,
+    pub is_mutable: bool,
+}
