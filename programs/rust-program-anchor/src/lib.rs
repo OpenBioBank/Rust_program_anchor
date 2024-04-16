e0340674a0dcb68c64d3e887f914fb9bf6aacc8c
gph <1598542650@qq.com>
2024-04-16 22:50:56 +0800

    finish anchor version

--- programs/rust-program-anchor/src/lib.rs
+++ programs/rust-program-anchor/src/lib.rs
@@ -6,6 +6,7 @@ declare_id!("F7uwK9BXr7orrQWJujdoTVbd7P7yMDnPA84udYYyPPLM");
 
 #[program]
 pub mod rust_program_anchor {
+
     use super::*;
 
     pub fn initialize_mint_account(
@@ -20,6 +21,36 @@ pub mod rust_program_anchor {
         cid: String,
         is_mutable: bool,
     ) -> Result<()> {
+        let metadata_account = &mut ctx.accounts.metadata_account;
+        
+        metadata_account.id = id;
+        metadata_account.description = description;
+        metadata_account.owner = owner;
+        metadata_account.creator = creator;
+        metadata_account.authorize = authorize;
+        metadata_account.url = url;
+        metadata_account.is_initialized = is_initialized;
+        metadata_account.cid = cid.clone();
+        metadata_account.is_mutable = is_mutable;
+
+        Ok(())
+    }
+
+    pub fn create_nft(
+        ctx: Context<CreateNft>,
+        cid: String,
+    ) -> Result<()> {
+        mint_to(
+            CpiContext::new_with_signer(
+                ctx.accounts.token_program.to_account_info(),
+                MintTo {
+                    authority: ctx.accounts.mint.to_account_info(),
+                    to: ctx.accounts.token_account.to_account_info(),
+                    mint: ctx.accounts.mint.to_account_info(),
+                },
+                &[&[ctx.accounts.initializer.key().as_ref(),cid.as_bytes(),&[ctx.bumps.mint]]]),
+                1,
+            )?;
         Ok(())
     }
 }
@@ -58,6 +89,30 @@ pub struct InitializeMintAccount<'info> {
     pub initializer: Signer<'info>,
     pub system_program: Program<'info, System>,
     pub token_program: Program<'info, Token>,
+    #[account(
+        init_if_needed,
+        payer = initializer,
+        associated_token::mint = mint,
+        associated_token::authority = initializer
+    )]
+    pub token_account: Account<'info, TokenAccount>,
+    pub associated_token_program: Program<'info, AssociatedToken>,
+    pub rent: Sysvar<'info, Rent>
+}
+
+#[derive(Accounts)]
+#[instruction(cid:String)]
+pub struct CreateNft<'info> {
+    #[account(
+        seeds = [initializer.key().as_ref(),cid.as_bytes()],
+        bump,
+        mut
+    )]
+    pub mint: Account<'info, Mint>,
+    #[account(mut)]
+    pub initializer: Signer<'info>,
+    pub token_program: Program<'info, Token>,
+    pub system_program: Program<'info, System>,
     #[account(
         init_if_needed,
         payer = initializer,
